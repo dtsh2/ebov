2f7f2712ed4f97eaa44212508ffdc3351a6d09b4
David Hayman <davidtshayman@gmail.com>
2017-05-24 18:54:48 +1200

    revised images

--- mapping.R
+++ mapping.R
@@ -198,7 +198,166 @@ legend("bottomleft",#-180.1516,90,
 ##
 dev.off()
 
+###
+
+RES$sp<-RES$Positive/RES$Number
+RES$sp_scale<-RES$sp/RES$Number
+ebov<- RES[RES$Genus == "EBOV", ]
+marv<- RES[RES$Genus == "MARV", ]
+
+ebov$Country[2]<-"Burkina Faso"
+ebov$Country[4]<-"Central African Republic"
+ebov$Country[10]<-"Sierra Leone"
+
+marv$Country[2]<-"Central African Republic"
+marv$Country[6]<-"Sierra Leone"
+#
+ebov$country <- c("BEN","BFA","CMR",'CAF','TCD',"GIN","LBR","NGA",
+                  "SEN","SLE")
+marv$country <- c("CMR",'CAF','TCD',"LBR","NGA",
+                  "SLE")
+
+# These are the ISO3 names of the countries you'd like to plot in red
+# merge to the map data
+
+malERsp <- joinCountryData2Map(ebov, joinCode = "ISO3",
+                               nameJoinColumn = "country")
+#
+# This will join your malDF data.frame to the country map data
+pdf("ebov_sp_results.pdf", width=8, height=6)
+
+mapCountryData(malMap, nameColumnToPlot="West_Africa", 
+               catMethod = "categorical",
+               mapTitle="",addLegend=F,
+               missingCountryCol = "wheat",
+               mapRegion="world",
+               xlim=c(-15,20),
+               ylim=c(-5,25),
+               oceanCol = "lightblue",
+               colourPalette = "terrain"
+)
+
+mapParams<-mapCountryData(malERsp, nameColumnToPlot="sp", 
+                          catMethod = "categorical",
+                          mapTitle="Risk based on anti-Ebolavirus seroprevalence results",
+                          addLegend=F,
+                          #missingCountryCol = "wheat",
+                          mapRegion="world",
+                          xlim=c(-15,20),
+                          ylim=c(-5,25),
+                          oceanCol = "lightblue",
+                          colourPalette = "heat",
+               add=T)
+
 ##
+dev.off()
+
+max(marv$sp)/max(ebov$sp)
+
+malMRsp <- joinCountryData2Map(marv, joinCode = "ISO3",
+                               nameJoinColumn = "country")
+#
+# This will join your malDF data.frame to the country map data
+pdf("marv_sp_results.pdf", width=8, height=6)
+
+mapCountryData(malMap, nameColumnToPlot="West_Africa", 
+               catMethod = "categorical",
+               mapTitle="",addLegend=F,
+               missingCountryCol = "wheat",
+               mapRegion="world",
+               xlim=c(-15,20),
+               ylim=c(-5,25),
+               oceanCol = "lightblue",
+               colourPalette = "terrain"
+)
+
+mapParams<-mapCountryData(malMRsp, nameColumnToPlot="sp", 
+                          catMethod = "categorical",
+                          mapTitle="Anti-Marburg seroprevalence",
+                          addLegend=F,
+                          #missingCountryCol = "wheat",
+                          mapRegion="world",
+                          xlim=c(-15,20),
+                          ylim=c(-5,25),
+                          oceanCol = "lightblue",
+                          colourPalette = c("#FFFFD5FF", # 1
+                                            "#FFFF80FF", # 2
+                                            "#FFFF2AFF", # 3
+                                            "#FFFF00FF", # 4
+                                            "#FFDF00FF", # 5
+                                            "#FFBF00FF"), # 6
+                          #)
+                          add=T)
+dev.off()
+##
+## scaled by total tested
+pdf("ebov_sp_results_scaled.pdf", width=8, height=6)
+
+mapCountryData(malMap, nameColumnToPlot="West_Africa", 
+               catMethod = "categorical",
+               mapTitle="",addLegend=F,
+               missingCountryCol = "wheat",
+               mapRegion="world",
+               xlim=c(-15,20),
+               ylim=c(-5,25),
+               oceanCol = "lightblue",
+               colourPalette = "terrain"
+)
+
+mapParams<-mapCountryData(malERsp, nameColumnToPlot="sp_scale", 
+                          catMethod = "categorical",
+                          mapTitle="Risk based on anti-Ebolavirus seroprevalence results",
+                          addLegend=F,
+                          #missingCountryCol = "wheat",
+                          mapRegion="world",
+                          xlim=c(-15,20),
+                          ylim=c(-5,25),
+                          oceanCol = "lightblue",
+                          colourPalette = "heat",
+                          add=T)
+
+##
+dev.off()
+
+max(marv$sp)/max(ebov$sp)
+
+malMRsp <- joinCountryData2Map(marv, joinCode = "ISO3",
+                               nameJoinColumn = "country")
+#
+# This will join your malDF data.frame to the country map data
+pdf("marv_sp_results_scaled.pdf", width=8, height=6)
+
+mapCountryData(malMap, nameColumnToPlot="West_Africa", 
+               catMethod = "categorical",
+               mapTitle="",addLegend=F,
+               missingCountryCol = "wheat",
+               mapRegion="world",
+               xlim=c(-15,20),
+               ylim=c(-5,25),
+               oceanCol = "lightblue",
+               colourPalette = "terrain"
+)
+
+mapParams<-mapCountryData(malMRsp, nameColumnToPlot="sp_scale", 
+                          catMethod = "categorical",
+                          mapTitle="Anti-Marburg seroprevalence",
+                          addLegend=F,
+                          #missingCountryCol = "wheat",
+                          mapRegion="world",
+                          xlim=c(-15,20),
+                          ylim=c(-5,25),
+                          oceanCol = "lightblue",
+                          colourPalette = c("#FFFFD5FF", # 1
+                                            "#FFFF80FF", # 2
+                                            "#FFFF2AFF", # 3
+                                            "#FFFF00FF", # 4
+                                            "#FFDF00FF", # 5
+                                            "#FFBF00FF"), # 6
+                          #)
+                          add=T)
+dev.off()
+##
+
 data_tot<-aggregate(f_data$Number~ f_data$Country,FUN=sum)
 data_tot$Country<-c("BEN","BFA","CMR","CAF","TCD","CIV",
                   "GIN","LBR","NGA",
@@ -233,3 +392,5 @@ mapParams<-mapCountryData(malMap, nameColumnToPlot="Number",
 #adding a modified legend by specifying extra parameters
 do.call( addMapLegendBoxes, c(mapParams,title="Numbers",cex=0.8))
 dev.off()
+
+##
